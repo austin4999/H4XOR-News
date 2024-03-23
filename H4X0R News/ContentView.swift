01ac51e03105c4e5e7c83d1d38ba483f84a1940f
Austin Smith <austin4999@gmail.com>
2024-03-23 09:12:19 -0500

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Austin Smith on 3/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Austin Smith on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
